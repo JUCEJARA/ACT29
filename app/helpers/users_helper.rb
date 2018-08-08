8107787ae2e1d0ec9715d1d94de7374a89d901ef
julio Cesar jara ramirez <juliocesar216@gmail.com>
2018-08-07 20:58:16 -0400

    creado controller sessions

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+    def logged?
+        session[:user_id].present? ? true : false
+    end
+    def current_user
+        logged? ? User.find(session[:user_id]) : nil
+    end 
 end
